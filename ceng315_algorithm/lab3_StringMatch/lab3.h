2d82b520ff0789643c06aa9628285f3d02441cf5
batyrchary <batyrchary95@gmail.com>
2019-05-24 17:09:26 -0700

    Remove duplicated directory

--- ceng315_algorithm/lab3_StringMatch/lab3.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-using namespace std;
-
-vector<unsigned> find_occurrences(char *T, unsigned text_size, char *P, unsigned pattern_size, char c1, char c2);
-
-// DO NOT modify above lines
